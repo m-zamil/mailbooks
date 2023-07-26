0c1407ce5614fb08bb605692c88aa322e3ee3b7a
Zamil <muzamil-javascriptdev@outlook.com>
2023-07-27 02:34:33 +0500

    completed

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-// JavaScript
+$(document).ready(function () {
+  $('.testimonials .wrapper').slick({
+    arrows: true,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 778,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
+});
+
+//Hamburger menu
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const navLink = document.querySelectorAll('.nav-link');
+
+hamburger.addEventListener('click', mobileMenu);
+navLink.forEach((n) => n.addEventListener('click', closeMenu));
+
+function mobileMenu() {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+}
+
+function closeMenu() {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}
